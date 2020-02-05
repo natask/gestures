4e4a0ec3243b5f86b48d4c9dea536c136aed4415
savnk_manjaro <savnk@manjaro.com>
2020-02-04 23:41:00 -0500

    did progress on customizability and wrote installer.
    - probablly need to detach my workflow from script to allow for flexiable
    customizability.
    - currently have gesture start fully customiziable under my workflow.
    - will need to replicate 8 directional movement for gesture update to make it
      workflow agnostic.

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+install_location=/usr/local/bin
+config_location=~/.config
+autostart_location=~/.config/autostart
+
+# remove placed files
+sudo rm ${install_location}/gestures 
+sudo rm ${install_location}/evemu_do 
+sudo rm ${install_location}/getConfig.py
+rm ${config_location}/gestures.conf 
+rm ${autostart_location}/gestures.desktop 
+
