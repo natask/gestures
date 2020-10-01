25026de70d7a396a76bb1a8fb18fe8b643206a61
savnk_manjaro <savnk@manjaro.com>
2020-09-30 19:17:57 -0700

    (fix): installation script after deprecating evemu

--- install.sh
+++ install.sh
@@ -31,10 +31,29 @@ cat pkg_requirements | xargs -n 1 sudo ${pkgM} ${install} ${auto}
 echo "Installing $(sed -n -e 'H;${x;s/\n/, /g;s/^,[ ]//;p;}' py_requirements)."
 cat py_requirements | xargs -n 1 sudo pip3 install
 
+
+echo ""
+echo ""
+# create locations if they don't exist.
+if test ! -d "${config_location}"; then
+        echo "creating ${config_location}"; mkdir -p ${config_location}
+fi
+
+if test ! -d "${autostart_location}"; then
+        echo "creating ${autostart_location}"; mkdir -p ${autostart_location}
+fi
+
+if test ! -d "${install_location}"; then
+        echo "creating ${install_location}"; mkdir -p ${install_location}
+fi
+
+echo ""
 # place files in corresponding locations
 echo "Placing gestures getConfig.py in ${install_location}."
 sudo cp gestures getConfig.py ${install_location}
 
+echo ""
+echo ""
 while true; do
     echo "Should I install utilities, $(ls -m utilities), in ${install_location}? Recommended for default operation."
     read -p "(y/n):" choice
@@ -45,26 +64,34 @@ while true; do
     esac
 done
 
-
+echo ""
+echo ""
 if test -f "${config_location}/gestures.conf"; then
     while true; do
         echo "Config file exists. override (saves backup as gestures.conf.bak)?" 
         read -p "(y/n):" choice
-        case "$choice" in 
-		y|Y )  echo "Would you like xdo-tool version (x or y) or evemu_do version (e or n)? xdo-tool version is recommended unless using wayland."
-			read -p "([x/y]do-tool/[e/n]vemu_do):" version
-			case "$version" in
-				x|X|y|Y ) echo "Placing xdo-tool version of config file in ${config_location} while making a temporary backup.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_xdo_tool.conf ${config_location}/gestures.conf; break;;
-				e|E|n|N ) echo "Placing evemu_do version of config file in ${config_location} while making a temporary backup.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_evemu_do.conf ${config_location}/gestures.conf; break;;
+        case "$choice" in
+                y|Y )  echo "Would you like xdo-tool version (x or y) or evemu_do version (e or n)? xdo-tool version is recommended unless using wayland."
+                        read -p "([x/y]do-tool/[e/n]vemu_do):" version
+                        case "$version" in
+                                x|X|y|Y ) echo "Placing xdo-tool version of config file in ${config_location} while making a temporary backup.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_xdo_tool.conf ${config_location}/gestures.conf; break;;
+                                e|E|n|N ) echo "Placing evemu_do version of config file in ${config_location} while making a temporary backup.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_evemu_do.conf ${config_location}/gestures.conf; break;;
+                        esac; break;;
           n|N ) echo "Skipping config file placement."; break;;
           * ) echo "Please answer y or n.";;
         esac
     done
 else
-    echo "Placing config file in ${config_location}."
-    cp gestures.conf ${config_location};  
+                         echo "Would you like xdo-tool version (x or y) or evemu_do version (e or n)? xdo-tool version is recommended unless using wayland."
+                        read -p "([x/y]do-tool/[e/n]vemu_do):" version
+                        case "$version" in
+                                x|X|y|Y ) echo "Placing xdo-tool version of config file in ${config_location}.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_xdo_tool.conf ${config_location}/gestures.conf; break;;
+                                e|E|n|N ) echo "Placing evemu_do version of config file in ${config_location}.";cp ${config_location}/gestures.conf ${config_location}/gestures.conf.bak; cp gestures_evemu_do.conf ${config_location}/gestures.conf; break;;
+                        esac;
 fi
 
+echo ""
+echo ""
 echo "Placing desktop file in ${autostart_location}."
 cp gestures.desktop ${autostart_location}
 
